d30551f4b0c1ebc6ec0240ae906fffe355a84ecc
Gabriel <gabrieldiasbnu@hotmail.com>
2020-11-18 22:57:12 +0100

    refactor: replace expect toBe(null) for toBeNull()

--- src/data/usecases/authentication/db-authentication.spec.ts
+++ src/data/usecases/authentication/db-authentication.spec.ts
@@ -70,7 +70,7 @@ describe('DbAuthentication UseCase', () => {
     const { sut, loadAccountByEmailRepositoryStub } = makeSut()
     jest.spyOn(loadAccountByEmailRepositoryStub, 'load').mockReturnValueOnce(null)
     const accessToken = await sut.auth(makeFakeAuthentication())
-    expect(accessToken).toBe(null)
+    expect(accessToken).toBeNull()
   })
 
   test('Should call HashComparer with correct values', async () => {
