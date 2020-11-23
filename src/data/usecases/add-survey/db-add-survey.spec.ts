728e8d2d28d388a287ae8d2ca78a179add7a0ba8
Gabriel <gabrieldiasbnu@hotmail.com>
2020-11-23 22:00:09 +0100

    test: ensure DbAddSurvey throws an exception if AddSurveyRepository also throws

--- src/data/usecases/add-survey/db-add-survey.spec.ts
+++ src/data/usecases/add-survey/db-add-survey.spec.ts
@@ -39,4 +39,11 @@ describe('DbAddSurvey UseCase', () => {
     await sut.add(surveyData)
     expect(addSpy).toHaveBeenCalledWith(surveyData)
   })
+
+  test('Should throw an exception if AddSurveyRepository also throws one', async () => {
+    const { sut, addSurveyRepositoryStub } = makeSut()
+    jest.spyOn(addSurveyRepositoryStub, 'add').mockReturnValueOnce(new Promise((resolve, reject) => reject(new Error())))
+    const promise = sut.add(makeFakeSurveyData())
+    await expect(promise).rejects.toThrow()
+  })
 })
