261ce518476637a49d2ad4c649b27a1d474e20de
Gabriel <gabrieldiasbnu@hotmail.com>
2020-12-27 21:49:11 +0100

    docs: add save survey result api docs

---
+++ src/main/docs/paths/survey-result-path.ts
@@ -0,0 +1,44 @@
+export const surveyResultPath = {
+  put: {
+    tags: ['Survey'],
+    summary: 'Create a survey answer',
+    parameters: [{
+      in: 'path',
+      name: 'surveyId',
+      required: true,
+      schema: {
+        type: 'string'
+      }
+    }],
+    requestBody: {
+      content: {
+        'application/json': {
+          schema: {
+            $ref: '#/schemas/saveSurveyParams'
+          }
+        }
+      }
+    },
+    responses: {
+      200: {
+        description: 'Success',
+        content: {
+          'application/json': {
+            schema: {
+              $ref: '#/schemas/surveyResult'
+            }
+          }
+        }
+      },
+      403: {
+        $ref: '#/components/forbidden'
+      },
+      404: {
+        $ref: '#/components/notFound'
+      },
+      500: {
+        $ref: '#/components/serverError'
+      }
+    }
+  }
+}
